b0abf013c9f9b67bec9de099a861571a08cd3808
Chris-Han3en <75200147+Chris-Han3en@users.noreply.github.com>
2021-03-31 22:43:49 +0100

    Add files via upload

---
+++ Client/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Threading;
+
+class Program
+{
+    public static string Directory = AppDomain.CurrentDomain.BaseDirectory;
+
+    public static bool isRunning = false;
+
+
+    static void Main(string[] args)
+    {
+        isRunning = true;
+
+        Thread mainThread = new Thread(new ThreadStart(MainThread));
+        mainThread.Start();
+
+        Client.ConnectToServer();
+        Console.WriteLine("Don't close this window!");
+    }
+    private static void MainThread()
+    {
+        DateTime _nextLoop = DateTime.Now;
+
+        while (isRunning)
+        {
+            while (_nextLoop < DateTime.Now)
+            {
+                // If the time for the next loop is in the past, aka it's time to execute another tick
+                ThreadManager.UpdateMain();
+
+                _nextLoop = _nextLoop.AddMilliseconds(Constants.MS_PER_TICK); // Calculate at what point in time the next tick should be executed
+
+                if (_nextLoop > DateTime.Now)
+                {
+                    // If the execution time for the next tick is in the future, aka the server is NOT running behind
+                    Thread.Sleep(_nextLoop - DateTime.Now); // Let the thread sleep until it's needed again.
+                }
+            }
+        }
+    }
+}
---
+++ Client/ThreadManager.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+class ThreadManager
+{
+    private static readonly List<Action> executeOnMainThread = new List<Action>();
+    private static readonly List<Action> executeCopiedOnMainThread = new List<Action>();
+    private static bool actionToExecuteOnMainThread = false;
+
+    // Sets an action to be executed on the main thread.
+    public static void ExecuteOnMainThread(Action _action)
+    {
+        if (_action == null)
+        {
+            Console.WriteLine("No action to execute on main thread!");
+            return;
+        }
+
+        lock (executeOnMainThread)
+        {
+            executeOnMainThread.Add(_action);
+            actionToExecuteOnMainThread = true;
+        }
+    }
+
+    // Executes all code meant to run on the main thread. NOTE: Call this ONLY from the main thread.
+    public static void UpdateMain()
+    {
+        if (actionToExecuteOnMainThread)
+        {
+            executeCopiedOnMainThread.Clear();
+            lock (executeOnMainThread)
+            {
+                executeCopiedOnMainThread.AddRange(executeOnMainThread);
+                executeOnMainThread.Clear();
+                actionToExecuteOnMainThread = false;
+            }
+
+            for (int i = 0; i < executeCopiedOnMainThread.Count; i++)
+            {
+                executeCopiedOnMainThread[i]();
+            }
+        }
+    }
+}
